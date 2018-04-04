cef815b713e19b1fb0b5b6cfc42e4a31da19d082
unknown <josecarlosvieira@tecnico.ulisboa.pt>
2018-04-04 17:23:23 +0100

    ps only

---
+++ project_ps_only/project_ps_only.ip_user_files/sim_scripts/design_1/ies/run.f
@@ -0,0 +1,33 @@
+-makelib ies_lib/xil_defaultlib -sv \
+  "C:/Xilinx/Vivado/2017.4/data/ip/xpm/xpm_fifo/hdl/xpm_fifo.sv" \
+  "C:/Xilinx/Vivado/2017.4/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+  "C:/Xilinx/Vivado/2017.4/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+-endlib
+-makelib ies_lib/xpm \
+  "C:/Xilinx/Vivado/2017.4/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies_lib/axi_infrastructure_v1_1_0 \
+  "../../../../project.srcs/sources_1/bd/design_1/ipshared/ec67/hdl/axi_infrastructure_v1_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/smartconnect_v1_0 -sv \
+  "../../../../project.srcs/sources_1/bd/design_1/ipshared/02c8/hdl/sc_util_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/axi_protocol_checker_v2_0_1 -sv \
+  "../../../../project.srcs/sources_1/bd/design_1/ipshared/3b24/hdl/axi_protocol_checker_v2_0_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/axi_vip_v1_1_1 -sv \
+  "../../../../project.srcs/sources_1/bd/design_1/ipshared/a16a/hdl/axi_vip_v1_1_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/processing_system7_vip_v1_0_3 -sv \
+  "../../../../project.srcs/sources_1/bd/design_1/ipshared/1313/hdl/processing_system7_vip_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_processing_system7_0_0/sim/design_1_processing_system7_0_0.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/design_1/sim/design_1.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
---
+++ project_ps_only/project_ps_only.sdk/ps_only/Debug/src/ps_only.d
@@ -0,0 +1,13 @@
+src/ps_only.o src/ps_only.o: ../src/ps_only.c \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xtime_l.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xtime_l.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h:
